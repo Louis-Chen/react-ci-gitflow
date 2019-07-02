821a02f77c19af9747777c77f2c47355e6086435
LuisChen <leo5916267@gmail.com>
2019-07-02 12:27:26 +0800

    刪除sum ,test及components 分開

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Counter } from "./Counter";
+import { Counter } from "../components/Counter";
 import { render, cleanup } from "@testing-library/react";
 
 afterEach(cleanup);
