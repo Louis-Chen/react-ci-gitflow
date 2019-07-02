7a4f0266579a8901da30e18054e37fab4695fae3
LuisChen <leo5916267@gmail.com>
2019-07-02 15:26:21 +0800

    修改 架構,新增 Button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Counter } from "./components/Counter";
+import React from 'react'
+import './App.css'
+
+import { Counter } from './components/Counter'
+import { Button } from './components/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
         <Counter number={30} />
+        <hr />
+        <Button />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
